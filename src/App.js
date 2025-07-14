b1602c79f0a63c0f97a888684d75eca10b8cb086
[Justin Cupp] <[jcupp35@gmail.com]>
2025-07-14 11:13:25 -0500

    updated h1 text

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [num, setNum] = useState(0);
   return (
     <div className="app-container">
-      <h1 className="counter-heading">Current number: {num}</h1>
+      <h1 className="counter-heading">Current Number: {num}</h1>
       <div className="button-container">
         <button className="counter-button" onClick={() => setNum(num + 1)}>Add 1</button>
         <button className="counter-button" onClick={() => setNum(num -1)}>Substract 1</button>
