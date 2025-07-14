1af78e687547b939a599e1ffb61fbd9bc5b1c7bb
[Justin Cupp] <[jcupp35@gmail.com]>
2025-07-14 11:10:00 -0500

    updated h1 and button classes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
   const [num, setNum] = useState(0);
   return (
     <div className="app-container">
-      <h1>Current number: {num}</h1>
+      <h1 className="counter-heading">Current number: {num}</h1>
       <div className="button-container">
-        <button onClick={() => setNum(num + 1)}>Add 1</button>
-        <button onClick={() => setNum(num -1)}>Substract 1</button>
+        <button className="counter-button" onClick={() => setNum(num + 1)}>Add 1</button>
+        <button className="counter-button" onClick={() => setNum(num -1)}>Substract 1</button>
       </div>
     </div>
   );
